813deaf307cbdfda96d6dd61d595713bff58894f
App Academy Student <student@appacademy.io>
2016-04-06 17:13:10 -0700

    Minor fix with ::where functionality

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -5,7 +5,7 @@ module Searchable
   def where(params)
     where_line = params.keys.map {|key| "#{key} = ?"}.join(" AND ")
 
-    results = DBConnection.execute(<<-SQL, *params.values)
+    results = DBConnection.execute(<<-SQL, params.values)
       SELECT
         *
       FROM
